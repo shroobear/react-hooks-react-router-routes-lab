b3a03597ec092021c5f36ddbdb8841092c2fa7a2
Stanley Shrewsbery <homestanrunner@gmail.com>
2023-06-13 15:51:38 -0400

    lab complete

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,19 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const renderedActors = actors.map((actor) => (
+    <div key={actor.name}>
+      Name: {actor.name} <br/> Movies:
+      <ul>
+        {actor.movies.map((movie) => <li key={movie}>{movie}</li>)}
+      </ul>
+    </div>
+  ));
+  return (
+  <div>
+    <h1>Actors Page</h1>
+    {renderedActors}
+  </div>);
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const renderedDirectors = directors.map((director) => (
+    <div key={director.name}>
+      Name: {director.name} <br/> Movies:
+      <ul>
+        {director.movies.map((movie) => <li key={movie}>{movie}</li>)}
+      </ul>
+    </div>
+  ));
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {renderedDirectors}
+  </div>
+  );
 }
 
 export default Directors;
